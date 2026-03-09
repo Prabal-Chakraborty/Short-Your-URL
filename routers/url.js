d0b10942e85f2f1e193ef1f9c401bda0f1d5dcdb
Prabal Chakraborty <122000141+Prabal-Chakraborty@users.noreply.github.com>
2026-03-09 21:43:28 +0530

    Delete routers directory

--- routers/url.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { handleGenerateShortUrl,handleGetAnalytics } = require('../controllers/url');
-
-const router = express.Router();
-
-router.post('/', handleGenerateShortUrl);
-router.get('/analytics/:shortId',handleGetAnalytics)
-
-module.exports = router;
